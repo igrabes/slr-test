87e607de145f54231f19151ef47d55e028910917
Ian <iangrabill@gmail.com>
2012-10-12 09:51:42 -0400

    request

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -3,11 +3,12 @@ class HooksController < ApplicationController
 
   def receiver
 
-    @data_json = JSON.parse(request.body.read)
+    @data = request.body
+    # @data_json = JSON.parse(request.body.read)
 
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    # respond_to do |format|
+      # format.html
+      # format.json
+    # end
   end
 end
