57f9748be7a5a518e550d6a0a882ff95be688aa8
Ian <iangrabill@gmail.com>
2012-10-12 13:12:38 -0400

    test

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,14 +4,17 @@ class HooksController < ApplicationController
   def receiver
 
     @data = request.body.read
+    Rails.logger.info "********** #{@data}"
+    Rails.logger.info "********** #{@data['merge_id']}"
+    Rails.logger.info "********** #{@data['file_name']}"
     @merge_id = @data['merge_id']
     @file_name = @data['file_name']
     @file = @data['file_contents']
     # @data_json = JSON.parse(request.body.read)
 
-    # respond_to do |format|
-      # format.html
-      # format.json
-    # end
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 end
